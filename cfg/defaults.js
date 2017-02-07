2de1d0dc2d9321663a8af2b23d465b44762e8bc2
Federico Iosue <federico.iosue@dada.eu>
2017-02-07 16:35:43 +0100

    Added bootstrap SASS

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -46,7 +46,7 @@ function getDefaultModules() {
       },
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
+        loader: 'url-loader?limit=888192'
       },
       {
         test: /\.(mp4|ogg|svg)$/,
